ed984f794a308d4cf74b490e81439509dacb06bd
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-28 00:38:54 +0000

    Initial commit - test data for tsbles

---
+++ db/seeds/1_polls.sql
@@ -0,0 +1,6 @@
+INSERT INTO polls (email, title, description, administrative_link, submission_link)
+VALUES ('alice@gmmail.com', 'Weekend activity', 'Hey folks please vote for what we should do for the weekend.', 'http://localhost:8080/result/xY78f4', 'http://localhost:8080/1');
+INSERT INTO polls (email, title, description, administrative_link, submission_link)
+VALUES ('mario@nintendo', 'Should I save the princess?', 'Like the title says. Personally I dont want to tbh.', 'http://localhost:8080/result/aC78fD', 'http://localhost:8080/2');
+INSERT INTO polls (email, title, description, administrative_link, submission_link)
+VALUES ('elon.musk@tesla', 'What should I tweet about?', 'You get to decide what I tweet about next. - <3 Musk', 'http://localhost:8080/result/Bn9gRP', 'http://localhost:8080/3');
---
+++ db/seeds/2_option_results.sql
@@ -0,0 +1,24 @@
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (1, 'Hiking', 11);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (1, 'Biking', 6);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (1, 'Go Karting', 15);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (1, 'Organized Crime', 1);
+
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (2, 'Yes', 87);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (2, 'No', 87);
+
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (3, 'SpaceX', 34);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (3, 'Tesla', 7);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (3, 'Doge Coin', 280);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (3, 'Memes', 0);
+INSERT INTO option_results (poll_id, option_name, option_value)
+VALUES (3, 'Stonk Market', 87);
