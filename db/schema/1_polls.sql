16dee95246959204c017277ea8486aaaecf4383f
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-28 00:37:17 +0000

    Initial commit - reset polls table

---
+++ db/schema/1_polls.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS polls CASCADE;
+
+CREATE TABLE polls (
+  id SERIAL PRIMARY KEY NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  administrative_link VARCHAR(255),
+  submission_link VARCHAR(255)
+);
