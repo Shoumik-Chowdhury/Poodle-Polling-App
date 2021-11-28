48640cf9119e16a2c9868c08dd9cd8de6f7dc795
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-28 00:37:48 +0000

    Initial commit - reset option_results table

---
+++ db/schema/2_option_results.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS option_results CASCADE;
+
+CREATE TABLE option_results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  poll_id INTEGER NOT NULL REFERENCES polls(id) ON DELETE CASCADE,
+  option_name VARCHAR(255),
+  option_value INTEGER NOT NULL
+);
