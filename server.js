ef7c33ade0fb35fc3d4b3713a9c42f65e756d171
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-29 22:58:59 +0000

    add app.use for /result

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const resultRoutes = require("./routes/results");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/results", resultRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -60,9 +62,9 @@ app.get("/poll/:id", (req, res) => {
   res.render("links");
 });
 
-app.get("/result/:adminid", (req, res) => {
-  res.render("links");
-});
+// app.get("/result/:adminid", (req, res) => {
+//   res.render("links");
+// });
 
 
 app.post("/links", (req, res) => {
