385c270f975ad504041120462375792e0b3aacd7
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-29 18:56:28 +0000

    Borda count implement, returns object with proper option_value

--- routes/poll.js
+++ routes/poll.js
@@ -6,7 +6,19 @@ router.use(bodyParser.urlencoded({extended: true}));
 module.exports = (db) => {//passed to server.js
   return router.post("/", (req, res) => {
 
-    console.log(req.body);
+    const rankArray = req.body.rank.decision_maker;
+    let optionArray = req.body.optionEntry;
+    let pointCount = rankArray.length;
+    for (const rank of rankArray) {
+      optionArray.forEach(element => {
+        if (element.option_name === rank) {
+          element.option_value = parseInt(element.option_value);
+          element.option_value += pointCount;
+          pointCount -= 1
+        }
+      });
+    };
+    console.log(optionArray);
 
   });
 };
