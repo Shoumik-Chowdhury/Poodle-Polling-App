ec810e76a74dd9244f2c83751047a52cb44df036
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-29 16:20:50 +0000

    Option strings in rank appear in console, proper format

--- routes/poll.js
+++ routes/poll.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({extended: true}));
 
 module.exports = (db) => {//passed to server.js
   return router.post("/", (req, res) => {
-    console.log(req.body);
+    let rankArray = req.body.decision_maker;
+    rankArray.forEach(element => {
+      console.log(element);
+    });
   });
 };
