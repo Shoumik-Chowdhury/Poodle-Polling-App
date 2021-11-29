a9447dc7d534c4a7294c3843412102553c034280
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-29 14:47:57 +0000

    connect options table to database

--- routes/links.js
+++ routes/links.js
@@ -10,27 +10,54 @@ const router = express.Router();
 
 
 module.exports = (db) => {//passed to server.js
-  return router.post("/", (req, res) => {
 
+  return router.post("/", (req, res) => {
+    const allOptions = [req.body.option1, req.body.option2, req.body.option3];
+    const arrayOfOptions = allOptions.filter((option) => option.length > 0);
+
+    //HELPER FUNCTION
     const generateRandomString = () => {
       return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
     };
+    ////////////////
     const link1admin = generateRandomString();
     const link2everyone = generateRandomString();
 
-    //insert into polls 
-    db.query(`
+    db.query(
+      `
     INSERT INTO polls (
       email, title, description, administrative_link, submission_link) 
       VALUES (
       $1, $2, $3, $4, $5)
       RETURNING *;
       `, [req.body.email, req.body.title, req.body.description, link1admin, link2everyone])
-      //insert into options
-      .then(result => {
-        console.log("RESULT DATABASE", result.rows[0]);
-        return result.rows[0];
+
+      .then(() => {
+
+        db.query(
+          `
+          SELECT *
+          FROM polls
+          WHERE administrative_link = $1
+          `, [link1admin])
+
+          .then(result => {
+
+            arrayOfOptions.forEach((option) => {
+              db.query(
+                `
+              INSERT INTO option_results (
+                poll_id, option_name, option_value) 
+                VALUES (
+                $1, $2, $3)
+                RETURNING *;
+                `, [result.rows[0]["id"], option, 0]
+
+              )
+            })
+          })
       })
+
       .catch(err => {
         res
           .status(500)
