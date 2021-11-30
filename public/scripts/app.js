d22dbd887417f027461480b2cc7c5bb08bbd488f
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-30 00:52:30 +0000

    update links

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,28 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+
+  $("#poll-form").on("submit", function(event) {
+    //preventing the default for submission of the form
+    event.preventDefault()
+    //takes the information from a form and transforms it into a string
+    const formInfo = $(this).serialize();
+
+    //form submission is handled here
+    $.post("/api/links", formInfo)
+      .then((res) => {
+        console.log(res)
+        const li = `<li>${res}</li>`
+        $("#links").append(li);
+
+        //handles reponse from the server
+        //res.send from links.js
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+
+  })
+});
