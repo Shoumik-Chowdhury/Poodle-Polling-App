7a2dbb9d95eedf0ca87d343c0c0420c66cf3dc61
Nicholas Yoshida <nyoshida44@gmail.com>
2021-12-02 01:37:36 +0000

    Colour changes for poll css

--- routes/pollRoute.js
+++ routes/pollRoute.js
@@ -5,7 +5,8 @@ module.exports =  (db) => {
   router.get("/:pollid", (req, res) => {
     let values = [req.params["pollid"]];
     db.
-      query(`SELECT option_results.* FROM option_results
+      query(`SELECT option_results.*, polls.title, polls.description
+            FROM option_results
             JOIN polls ON poll_id = polls.id
             WHERE submission_link = $1`, values)
       .then(response => {
