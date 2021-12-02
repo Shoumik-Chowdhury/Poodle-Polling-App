ccbf2e09bf0483de3acdfc5c96fbf8e3e44f7a1a
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-12-02 19:15:10 +0000

    change result plot to pie chart

--- public/scripts/poll_results.js
+++ public/scripts/poll_results.js
@@ -1,14 +1,36 @@
-google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+$(document).ready(() => {
 
-function drawChart() {
+  Chart.register(ChartDataLabels);
+  var xValues = xVal;
+  var yValues = yVal;
+  var barColors = ['#CB4335', '#1F618D', '#F1C40F', '#27AE60', '#884EA0', '#D35400'];
 
-  var data = google.visualization.arrayToDataTable(result);
-
-  var options = {
-    title:`${title}  \n Poll by: ${email}>`
-  };
-
-  var chart = new google.visualization.BarChart(document.getElementById('myChart'));
-    chart.draw(data, options);
-}
+  new Chart("myChart", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production"
+      },
+      plugins: {
+        datalabels: {
+          formatter: (value) => {
+            return value;
+          },
+          font: {
+            size: 18,
+          },
+          color: '#fff',
+          backgroundColor: '#404040'
+        }
+      }
+    }
+  });
+});
